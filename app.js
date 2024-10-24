60a6866438c39afe2e24adfad9c057f8f6cc632c
SETBACKFORCOMBACK <jd.surplus2006@gmail.com>
2024-10-24 19:09:02 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+
+// buttons will be refer to the wrap toggle 
+let btns =document.querySelectorAll('.wrap-toggle');
+
+
+let wraps =document.querySelectorAll('.wrap');
+
+for (let x=0; x < btns.length; x++) {
+ 
+    btns[x].addEventListener('click', function () {
+
+    wraps[x].classList.toggle('active');
+ 
+
+    })
+}
+
+
+
